2ef0a110587785830259df3eff293de1c42a47d8
Ryan Lott <ryanclott@outlook.com>
2022-07-16 20:42:04 -0500

    Finished exercise 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,39 +1,42 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import * as React from 'react';
 
-const useLocalStorageState = (key, defaultValue = '', {serialize = JSON.stringify, deserialize = JSON.parge} = {}) => {
+
+const useLocalStorageState = (key, defaultValue = '', {
+  serialize = JSON.stringify,
+  deserialize = JSON.parse
+} = {}) => {
   const [state, setState] = React.useState(() => {
     const valueInLocalStorage = window.localStorage.getItem(key)
-    if(valueInLocalStorage) {
-      return deserialize(valueInLocalStorage)
-    }
-    return defaultValue;
+    if (valueInLocalStorage) return deserialize(valueInLocalStorage);
+
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
   })
 
-  const prevKeyRef = React.useRef(key);
-
-  React.useEffect(()=> {
-    const prevKey = prevKeyRef.current
+  const prevKeyRef = React.useRef(key)
+  
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
     if(prevKey !== key) {
-      window.localStorage.removeItem(prevKey)
+      window.localStorage.removeItem(prevKey);
     }
-    prevKeyRef.current = key
-    window.localStorage.setItem(key, serialize(state))
-  }, [key, serialize, state]);
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, serialize, state]); 
+
   return [state, setState];
+
 }
-
-
 
 function Greeting({initialName = ''}) {
   const [name, setName] = useLocalStorageState('name', initialName)
 
-
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
